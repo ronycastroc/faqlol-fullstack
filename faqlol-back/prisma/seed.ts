f2879e8bfe93024e1aa4689fdd109c27b0b4d876
ronycastroc <castroroony@gmail.com>
2023-07-21 03:40:37 -0300

    feat: prisma seed faqs

---
+++ faqlol-back/prisma/seed.ts
@@ -0,0 +1,74 @@
+/* eslint-disable no-console */
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type ParentSection = {
+  name: string;
+  subSectionId: null;
+};
+
+type SubSection = {
+  name: string;
+  subSectionId: number;
+};
+
+const createRandomSections = async () => {
+  const parentSections: ParentSection[] = [];
+  const subSections: SubSection[] = [];
+  const subSubSections: SubSection[] = [];
+
+  for (let i = 1; i <= 5; i++) {
+    const parentSection: ParentSection = {
+      name: `Parent Section ${i}`,
+      subSectionId: null,
+    };
+    parentSections.push(parentSection);
+  }
+
+  for (const parentSection of parentSections) {
+    const createdParentSection = await prisma.section.create({
+      data: parentSection,
+    });
+
+    const subSection: SubSection = {
+      name: "Subsection 1",
+      subSectionId: createdParentSection.id,
+    };
+    subSections.push(subSection);
+
+    for (let i = 1; i <= 1; i++) {
+      const createdSubSection = await prisma.section.create({
+        data: subSections[subSections.length - 1],
+      });
+
+      const subSubSection: SubSection = {
+        name: `Subsubsection ${i}`,
+        subSectionId: createdSubSection.id,
+      };
+      subSubSections.push(subSubSection);
+    }
+  }
+
+  const allSections: (ParentSection | SubSection)[] = [
+    ...parentSections,
+    ...subSections,
+    ...subSubSections,
+  ];
+  await prisma.section.createMany({
+    data: allSections,
+  });
+};
+
+const main = async () => {
+  try {
+    await createRandomSections();
+    console.log("Seed completed successfully.");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+main();
